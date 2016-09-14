fef414c71c1b0b7f3fe57c95d7f832a8d247db29
polaris610 <polaris.tiger803@yandex.com>
2016-09-14 05:20:52 +0300

    survey report.

--- src/js/app/survey/controllers/dashboard.js
+++ src/js/app/survey/controllers/dashboard.js
@@ -64,63 +64,57 @@ app_survey.controller('DashboardSurveyController', [
 		that.embedCode = '';
 		$scope.GetEmbedTemp = function (temp) {
 			that.embedLink = '<a href="' + APP_SETTINGS.SURVEY_URL + temp.key_url + '">' + temp.temp_name + '</a>';
-			console.log(temp);
 			var tempCode = '<table align="center" width="100%" border="0" cellpadding="0" cellspacing="0">';
-			tempCode += '<tr>';
-			tempCode += '<td align="center" width="100%">';
+			tempCode += '<tr><td align="center" width="100%">';
 			tempCode += '<table align="center" style="color: #111; font-family: Helvetica, Arial, sans-serif;border-radius: 4px;border: 1px solid #ddd;max-width:600px;margin: 30px auto; padding: 20px;">';
 			tempCode += '<tr>';
-			tempCode += '<td align="center" style="font-weight: bold;font-size: 32px;padding-top: 25px;min-width: 400px;">' + temp.temp_header + '</td>';
+			tempCode += '<td align="center" style="padding-top: 25px;min-width: 400px;"><h1 style="font-weight: bold;font-size: 32px;">' + temp.temp_header + '</h1></td>';
 			tempCode += '</tr>';
 			if (temp.temp_image != '') {
-				tempCode += '<tr>';
-				tempCode += '<td>';
-				tempCode += '<a href="' + temp.temp_image + '">';
+				tempCode += '<tr><td><a href="' + temp.temp_image + '">';
 				tempCode += '<img src="' + temp.temp_image + '" alt="Click here show the image." style="cursor: pointer;vertical-align: middle;display: inline-block;max-width: 98%;height: auto;padding: 4px;line-height: 1.42857143;background-color: #fff;border: 1px solid #ddd;border-radius: 4px;-webkit-transition: all .2s ease-in-out;-moz-transition: all .2s ease-in-out;-ms-transition: all .2s ease-in-out;-o-transition: all .2s ease-in-out;transition: all .2s ease-in-out;">';
-				tempCode += '</a>';
-				tempCode += '</td>';
-				tempCode += '</tr>';
+				tempCode += '</a></td></tr>';
 			}
 			if (temp.section_id == 1 || temp.section_id == 2) {
-				tempCode += '<tr>';
-				tempCode += '<td align="center" style="padding-top: 25px;font-size: 22px;">' + temp.temp_header1 + '</td>';
-				tempCode += '</tr>';
+				tempCode += '<tr><td align="center" style="padding-top: 25px;"><h4 style="font-weight: normal;font-size: 22px;">' + temp.temp_header1 + '</h4></td></tr>';
 			}
 			if (temp.section_id == 1) {
 				for (var i in temp.temp_data) {
 					var p = i == 0 ? 25 : 5;
 					var href_flag = temp.key_url + '_COL_' + temp.temp_data[i].name + '_COL_' + i + '_COL_' + temp.temp_data[i].link;
 					var href_url = APP_SETTINGS.TARGET_URL + btoa(href_flag);
-					tempCode += '<tr>';
-					tempCode += '<td align="center" style="padding-top: ' + p + 'px;">';
+					tempCode += '<tr><td align="center" style="padding-top: ' + p + 'px;">';
 					tempCode += '<a href="' + href_url + '" style="min-width: 200px;display: inline-block;margin-top: 5px;color: #fff !important;background-color: #23b7e5;border-color: #23b7e5;padding: 10px 16px;font-size: 18px;line-height: 1.3333333;border-radius: 6px;cursor: pointer;text-align: center;text-decoration: none;">';
-					tempCode += temp.temp_data[i].name + '</a>';
-					tempCode += '</td>';
-					tempCode += '</tr>';
+					tempCode += temp.temp_data[i].name + '</a></td></tr>';
 				}
 			} else if (temp.section_id == 2) {
-				tempCode += '<tr><td><br/><br/></td></tr>';
-				
+				tempCode += '<tr><td><br/><br/></td></tr><tr><td height="47px" style="height: 47px;" align="center">';
+				for (var j in temp.temp_data.rateTemp) {
+					tempCode += '<div align="center" style="display: inline !important;vertical-align: middle;width: 47px;height: 45px;">';
+					var href_flag2 = temp.key_url + '_COL_' + temp.temp_data.rateTemp[j].name + '_COL_' + j + '_COL_' + temp.temp_data.rateTemp[j].link;
+					var href_url2 = APP_SETTINGS.TARGET_URL + btoa(href_flag2);
+					tempCode += '<a href="' + href_url2 + '" style="display: inline-block;color: #8d949c;font-size :18px !important; font-weight: normal; line-height: 1.3333333;text-decoration: none;cursor: pointer;padding: 10px 16px;margin-right: 2px;text-align: center;background-color: #fff;border: 1px solid #ccc;border-bottom: 2px solid #ccc;border-radius: 5px;-webkit-box-shadow: -1px 2px 2px #ddd;-moz-box-shadow: -1px 2px 2px #ddd;box-shadow: -1px 2px 2px #ddd;">';
+					tempCode += ((j * 1) + 1) + '</a>';
+					tempCode += '</div>';
+				}
+				tempCode += '</td></tr><tr><td><br/></td></tr><tr><td align="center" width="100%" style="padding: 3px 5%;">';
+				tempCode += '<div style="float: left;font-weight: normal;color: #8d949c;font-size: 14px;">' + temp.temp_data.rateLabel.left + '</div>';
+				tempCode += '<div style="float: right;font-weight: normal;color: #8d949c;">' + temp.temp_data.rateLabel.right + '</div>';
+				tempCode += '</td></tr>';
 			} else if (temp.section_id == 3) {
-				tempCode += '<tr><td><br/><br/></td></tr>';
-				tempCode += '<tr><td>';
-				tempCode += '<table align="center" width="100%" border="0">';
-				tempCode += '<tr>';
+				tempCode += '<tr><td><br/><br/></td></tr><tr><td>';
+				tempCode += '<table align="center" width="100%" border="0"><tr>';
 				tempCode += '<td align="right" style="padding-right: 20px;">';
 				var href_flag3 = temp.key_url + '_COL_' + temp.temp_data[0].name + '_COL_' + 0 + '_COL_' + temp.temp_data[0].link;
 				var href_url3 = APP_SETTINGS.TARGET_URL + btoa(href_flag3);
 				tempCode += '<a href="' + href_url3 + '" style="min-width: 100px;border-radius: 50px;color: #fff !important;background-color: #27c24c;border-color: #27c24c;font-weight: 500;outline: 0 !important;padding: 15px 25px;font-size: 18px;line-height: 1.3333333;text-align: center;cursor: pointer;text-decoration: none;display: inline-block;">';
-				tempCode += temp.temp_data[0].name + '</a>';
-				tempCode += '</td>';
+				tempCode += temp.temp_data[0].name + '</a></td>';
 				tempCode += '<td align="left" style="padding-left: 20px;">';
 				href_flag3 = temp.key_url + '_COL_' + temp.temp_data[1].name + '_COL_' + 1 + '_COL_' + temp.temp_data[1].link;
 				href_url3 = APP_SETTINGS.TARGET_URL + btoa(href_flag3);
 				tempCode += '<a href="' + href_url3 + '" style="min-width: 100px;border-radius: 50px;color: #fff !important;background-color: #f05050;border-color: #f05050;font-weight: 500;outline: 0 !important;padding: 15px 25px;font-size: 18px;line-height: 1.3333333;text-align: center;cursor: pointer;text-decoration: none;display: inline-block;">';
 				tempCode += temp.temp_data[1].name + '</a>';
-				tempCode += '</td>';
-				tempCode += '</tr>';
-				tempCode += '</table>';
-				tempCode += '</td></tr>';
+				tempCode += '</td></tr></table></td></tr>';
 			}
 			tempCode += '<tr><td><br/></td></tr>';
 			tempCode += '</table>';
